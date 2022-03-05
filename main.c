b4a80102528a346496b840886325fefb5496dbbb
Jakub Borkowski <bojakub@wp.pl>
2022-03-05 21:58:26 +0100

    Information displayed on console has been improved
    Comments became clearer.

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int allframes; //All frames of the file
-int freq; //The frequency of the frame change
-int change; //Change of frames
-int x,y; //Width, height
+int allframes; //Number of frames to process
+int freq; //Frequency of change determine how many frames should be modified after each other
+int change; //If '0' first frame will be modified. If '1' first modified frame will occur after number of frame specified in 'freq' variable
+int x,y; //Width and height of each frame in sequence
 char *taby; //Luminance table Y
 char *tabu; //Chroma table U
 char *tabv; //Chroma table V
 FILE *frb, *fwb; //Input and output file
 
 void processFrames(){
+    printf("\n");
     //Read and write logic
     for(int frame = 1; frame <= allframes; frame++){
         //Writing data from a file to tables
@@ -19,31 +20,31 @@ void processFrames(){
         fread(tabv, (size_t) (x / 4), (size_t) y, frb);
 
         //Loading from tables into a file
-        if(change%2==0){
+        if(change%2==0){//If true frame will be modified
             //Writing luminance Y data to a file
-            for(int i = (x*y)/2; i<x*y; i = i + x){
+            for(int i=(x*y)/2; i<x*y; i=i+x){
                 for(int j=0; j<x/2; j++) {
-                    fputc(taby[i + x/2 + j], fwb);
+                    fputc(taby[i+x/2+j], fwb);
                 }
                 for(int j=0; j<x/2; j++) {
                     fputc(taby[i + j], fwb);
                 }
             }
-            for(int i = 0; i<(x*y)/2; i = i + x){
+            for(int i=0; i<(x*y)/2; i=i+x){
                 for(int j=0; j<x/2; j++) {
-                    fputc(taby[i + x/2 + j], fwb);
+                    fputc(taby[i+x/2+j], fwb);
                 }
                 for(int j=0; j<x/2; j++) {
-                    fputc(taby[i + j], fwb);
+                    fputc(taby[i+j], fwb);
                 }
             }
             //Writing chrominance U data to a file
-            for(int i = (x*y)/8; i<(x*y)/4; i = i + x/2){
+            for(int i=(x*y)/8; i<(x*y)/4; i=i+x/2){
                 for(int j=0; j<x/4; j++) {
-                    fputc(tabu[i + x/4 + j], fwb);
+                    fputc(tabu[i+x/4+j], fwb);
                 }
                 for(int j=0; j<x/4; j++) {
-                    fputc(tabu[i + j], fwb);
+                    fputc(tabu[i+j], fwb);
                 }
             }
             for(int i = 0; i<(x*y)/8; i = i + x/2){
@@ -60,19 +61,19 @@ void processFrames(){
                     fputc(tabv[i + x/4 + j], fwb);
                 }
                 for(int j=0; j<x/4; j++) {
-                    fputc(tabv[i + j], fwb);
+                    fputc(tabv[i+j], fwb);
                 }
             }
             for(int i = 0; i<(x*y)/8; i = i + x/2){
                 for(int j=0; j<x/4; j++) {
-                    fputc(tabv[i + x/4 + j], fwb);
+                    fputc(tabv[i+x/4+j], fwb);
                 }
                 for(int j=0; j<x/4; j++) {
-                    fputc(tabv[i + j], fwb);
+                    fputc(tabv[i+j], fwb);
                 }
             }
         }
-        else{ //original image
+        else{ //Keep original image in frame
             fwrite(taby, (size_t) (x * y), 1, fwb);
             fwrite(tabu, (size_t) ((x * y) / 4), 1, fwb);
             fwrite(tabv, (size_t) ((x * y) / 4), 1, fwb);
@@ -80,29 +81,39 @@ void processFrames(){
         if(frame%freq==0) { //image change
             change++;
         }
-        printf("\rCompleted processing: %d of %d frames.", frame, allframes);
+        printf("\rCompleted processing: %d out of %d frames.", frame, allframes);
     }
+    printf("\n");
 }
 
 void checkFile(FILE *file, char *name){
     if (file == NULL){ //Checking the file
-        printf("Problem opening the %s file.", name);
+        printf("\nThe %s file cannot be opened.\n", name);
         exit(1);
     }
 }
 
 void initialize(int argc, char **argv){
+    printf("\n");
     if(argc == 8){
-        printf("Input: %s\n", argv[1]);
-        printf("Output: %s\n", argv[2]);
-        printf("Frames: %s\n", argv[3]);
-        printf("Frequency: %s\n", argv[4]);
-        printf("Resolution X: %s\n", argv[5]);
-        printf("Resolution Y: %s\n", argv[6]);
+        printf("Input         : %s\n", argv[1]);
+        printf("Output        : %s\n", argv[2]);
+        printf("Frames        : %s\n", argv[3]);
+        printf("Frequency     : %s\n", argv[4]);
+        printf("Resolution X  : %s\n", argv[5]);
+        printf("Resolution Y  : %s\n", argv[6]);
         printf("Start position: %s\n", argv[7]);
     }
     else{
-        printf("Wrong number of arguments!\nPlease provide exactly 7 arguments in the following order:\nInput file patch,\nOutput file patch,\nNumber of frames,\nFrequency of change,\nWidth,\nHeight,\nStart of change(0/1).");
+        printf("Wrong number of arguments!\n"
+               "Please provide exactly 7 arguments in the following order:\n"
+               "-Input file patch,\n"
+               "-Output file patch,\n"
+               "-Number of frames,\n"
+               "-Frequency of change,\n"
+               "-Width,\n"
+               "-Height,\n"
+               "-Start of change(0/1).\n");
         exit(0);
     }
 
